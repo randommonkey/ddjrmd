df40d32fe748a9732ef1d451da51543472e41d57
jpmarindiaz <jpmarindiaz@gmail.com>
2017-09-28 23:53:34 -0500

    fixed iframe sizes

--- inst/rmarkdown/templates/static/resources/static.js
+++ inst/rmarkdown/templates/static/resources/static.js
@@ -1,14 +1,44 @@
 $(document).ready(function() {
-    var footer = $('#footer').children().not('h1')
-    $('.footer-container').append(footer)
 
-        $('.owl-carousel').owlCarousel({
+    // Handle seealso
+    var seealso = {};
+    seealso.data = $('#see-also>.section.level2').map(function() {
+        var link = $(this).find('a')[0];
+        link = $(link).attr("href")
+        var title = $(this).find('h2')[0];
+        title = $(title).text()
+        var img = $(this).find('img')[0];
+        img = $(img).attr("src")
+        return ({ title: title, link: link, img: img })
+    });
+    // remove self
+    seealso.data = $.makeArray(seealso.data);
+    var url = window.location.href;
+    url = url.replace(/^.*\/|\.*$/g, '')
+    seealso.data = seealso.data.filter(function(el) {
+        return el.link != url
+    })
+    console.log(url)
+    console.log("seealso", seealso)
+    if(seealso.data.length > 0){
+      $('#seealsoContent').append(seealsoTemplate(seealso));    
+    }
+    // Remove all parsed sections
+    $('#see-also').find('h1').remove();
+    $('#see-also>.section.level2').remove();
+    
+    // Footer
+    $('.footer-container').append($('#footer'))
+    $('#footer').find('h1').remove()
+
+
+    $('.owl-carousel').owlCarousel({
         center: true,
         items: 2,
         loop: true,
         margin: 5,
         nav: true,
-        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
         responsive: {
             768: { items: 3 },
             992: { items: 3 },
@@ -16,4 +46,8 @@ $(document).ready(function() {
             1920: { items: 8 }
         }
     });
+
+    var footer = $('#footer').children().not('h1')
+    $('.footer-container').append(footer)
+
 });
--- inst/rmarkdown/templates/story/resources/story.js
+++ inst/rmarkdown/templates/story/resources/story.js
@@ -25,15 +25,18 @@ $(document).ready(function() {
     seealso.data = $.makeArray(seealso.data);
     var url = window.location.href;
     url = url.replace(/^.*\/|\.*$/g, '')
-    seealso.data = seealso.data.filter(function(el){
-      return el.link != url
+    seealso.data = seealso.data.filter(function(el) {
+        return el.link != url
     })
     console.log(url)
-    console.log("seealso",seealso)
-    $('#seealsoContent').append(seealsoTemplate(seealso));
+    console.log("seealso", seealso)
+    if(seealso.data.length > 0){
+      $('#seealsoContent').append(seealsoTemplate(seealso));    
+    }
     // Remove all parsed sections
     $('#see-also').find('h1').remove();
     $('#see-also>.section.level2').remove();
+    
     // Footer
     $('.footer-container').append($('#footer'))
     $('#footer').find('h1').remove()
